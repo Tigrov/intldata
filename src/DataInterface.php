17725618ccfa9a9dc01ba433ad4c79bd734fd0ee
Tigrov <rrr-r@ya.ru>
2017-07-15 18:44:23 +0300

    updated PHPDocs

--- src/DataInterface.php
+++ src/DataInterface.php
@@ -9,29 +9,29 @@ namespace tigrov\intldata;
 interface DataInterface
 {
     /**
-     * Returns list of codes.
-     * @return array list of codes.
+     * Returns list of codes
+     * @return array
      */
     public static function codes();
 
     /**
-     * Returns a boolean indicating whether data has a code.
+     * Returns a boolean indicating whether data has a code
      * @param string $code the code of the data
      * @return bool
      */
     public static function has($code);
 
     /**
-     * Returns list of names.
-     * @param string[]|null $codes list of codes to get names, the empty value means all codes
-     * @return array list of names with code keys [code => name]
+     * Returns list of names with code keys [code => name]
+     * @param string[]|null $codes the list of codes to get names, the empty value means all codes
+     * @return array
      */
     public static function names($codes = null);
 
     /**
-     * Returns name by code.
-     * @param string $code code
-     * @return string name
+     * Returns name by a code
+     * @param string $code the code
+     * @return string
      */
     public static function name($code);
 }
